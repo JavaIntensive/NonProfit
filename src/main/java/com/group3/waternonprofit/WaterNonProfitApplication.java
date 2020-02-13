1c22b8dbb41ae76dc2666f5379d788c9bbca6797
GBTC440018UR <GBTC440018UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-13 09:35:53 -0500

    First Commit

---
+++ src/main/java/com/group3/waternonprofit/WaterNonProfitApplication.java
@@ -0,0 +1,13 @@
+package com.group3.waternonprofit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterNonProfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterNonProfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group3/waternonprofit/WaterNonProfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.waternonprofit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterNonProfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
